f53ef99a5ad71a262753ddb7a643c9712b7ec6c8
PaulPintang <pauljpintang@gmail.com>
2022-04-16 10:25:29 +0800

    header layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Route, Routes, Switch } from "react-router-dom";
+// Mantine Components
+import { Container, MantineProvider } from "@mantine/core";
+// Components
 import LandingPage from "./pages/LandingPage";
+import Header from "./components/Header";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-    </Routes>
+    <MantineProvider emotionOptions={{ key: "mantine", prepend: false }}>
+      <Header />
+      <Routes>
+        <Route path="./" element={<LandingPage />} />
+      </Routes>
+    </MantineProvider>
   );
 }
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "@mantine/core";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center h-16 max-w-[1200px] mx-auto">
+      <div>
+        <h1 className="font-bold text-2xl text-[#293856s]">
+          <span className="text-[#4263EB]">DEV</span>Task
+        </h1>
+      </div>
+      <div className="flex items-center gap-3">
+        <Button variant="outline" color="indigo" size="xs">
+          Login
+        </Button>
+        <Button color="indigo" size="xs">
+          Sign up
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
