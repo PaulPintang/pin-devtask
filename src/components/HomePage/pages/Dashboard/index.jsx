057d8a289be36493262c55e9567ea36a90863816
PaulPintang <pauljpintang@gmail.com>
2022-04-19 09:03:43 +0800

    style: add dashboard cards

--- src/components/HomePage/pages/Dashboard/index.jsx
+++ src/components/HomePage/pages/Dashboard/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Grid } from "@mantine/core";
+import { FiStar } from "react-icons/fi";
+import { GiProgression } from "react-icons/gi";
+import { BiGitMerge } from "react-icons/bi";
 import Table from "./components/Table";
 const Dashboard = () => {
   return (
@@ -24,10 +27,15 @@ const Dashboard = () => {
               <div className="flex gap-3 items-center">
                 <div className="bg-yellow-300 w-2 h-2"></div>
                 <small className="text-gray-400  text-[10px] uppercase font-semibold ">
-                  All task
+                  All
                 </small>
               </div>
-
+              <div className="flex gap-3 items-center">
+                <div className="bg-indigo-300 w-2 h-2"></div>
+                <small className="text-gray-400  text-[10px] uppercase font-semibold ">
+                  New
+                </small>
+              </div>
               <div className="flex gap-3 items-center">
                 <div className="w-2 h-2 bg-blue-300"></div>
                 <small className="text-gray-400  text-[10px] uppercase font-semibold ">
@@ -47,40 +55,44 @@ const Dashboard = () => {
                 </small>
               </div>
             </div>
-            {/* <div className="w-full flex justify-between items-center gap-2">
+            <div className="w-full flex justify-between items-center gap-2">
               <div className=" bg-opacity-60 rounded-md shadow-md w-full">
-                <div className="h-[110px] text-center">
-                  <div className="h-full flex flex-col items-center justify-center">
-                    <h1 className="text-2xl text-gray-700">9 Tasks</h1>
-                    <div className="bg-yellow-300 w-8 h-2"></div>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-opacity-60 rounded-md shadow-md w-full">
-                <div className="h-[110px] text-center">
-                  <div className="h-full flex flex-col items-center justify-center">
-                    <h1 className="text-2xl text-gray-700">1 Task</h1>
-                    <div className="bg-blue-300 w-8 h-2"></div>
+                <div className="h-[80px] text-center p-4">
+                  <div className="bg-indigo-300 w-8 h-1"></div>
+                  <div className="h-full flex items-center justify-between">
+                    <div className="bg-gray-100 rounded-full p-2">
+                      <FiStar className="text-sm text-gray-400" />
+                    </div>
+                    <p className="text-[11px] text-gray-400">New Task</p>
+                    <p className="text-[25px]">07</p>
                   </div>
                 </div>
               </div>
               <div className=" bg-opacity-60 rounded-md shadow-md w-full">
-                <div className="h-[110px] text-center">
-                  <div className="h-full flex flex-col items-center justify-center">
-                    <h1 className="text-2xl text-gray-700">9 Task</h1>
-                    <div className="bg-green-300 w-8 h-2"></div>
+                <div className="h-[80px] text-center p-4">
+                  <div className="bg-blue-300 w-8 h-1"></div>
+                  <div className="h-full flex items-center justify-between">
+                    <div className="bg-gray-100 rounded-full p-2">
+                      <GiProgression className="text-sm text-gray-400" />
+                    </div>
+                    <p className="text-[11px] text-gray-400">In Progress</p>
+                    <p className="text-[25px]">01</p>
                   </div>
                 </div>
               </div>
               <div className=" bg-opacity-60 rounded-md shadow-md w-full">
-                <div className="h-[110px] text-center">
-                  <div className="h-full flex flex-col items-center justify-center">
-                    <h1 className="text-2xl text-gray-700">2 Tasks</h1>
-                    <div className="bg-red-300 w-8 h-2"></div>
+                <div className="h-[80px] text-center p-4">
+                  <div className="bg-red-300 w-8 h-1"></div>
+                  <div className="h-full flex items-center justify-between">
+                    <div className="bg-gray-100 rounded-full p-2">
+                      <BiGitMerge className="text-sm text-gray-400" />
+                    </div>
+                    <p className="text-[11px] text-gray-400">For QA</p>
+                    <p className="text-[25px]">03</p>
                   </div>
                 </div>
               </div>
-            </div> */}
+            </div>
           </div>
         </div>
       </Grid.Col>
