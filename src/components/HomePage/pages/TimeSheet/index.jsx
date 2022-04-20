3236b7db57096743e8d4e719fd103e8a3b6efe8b
PaulPintang <pauljpintang@gmail.com>
2022-04-20 10:52:43 +0800

    feat: expand table cell

--- src/components/HomePage/pages/TimeSheet/index.jsx
+++ src/components/HomePage/pages/TimeSheet/index.jsx
@@ -1,11 +1,29 @@
-import React from "react";
-import { FiMoreHorizontal } from "react-icons/fi";
+import React, { useState } from "react";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 const Timesheet = () => {
+  const [expand, setExpand] = useState(false);
+
   return (
     <div className="p-3">
       <p className="text-[12px] text-gray-400">Monday, April 18</p>
       <div className="flex flex-col">
         <p className="text-gray-700 text-2xl">Task Timesheets</p>
+      </div>
+      <div className="pt-3">
+        <div className="flex flex-col md:flex-row lg:flex-row lg:justify-start lg:items-center gap-2 md:gap-4 lg:gap-4 pb-3">
+          <div className="flex gap-3 items-center">
+            <div className="bg-yellow-300 w-2 h-2"></div>
+            <small className="text-gray-400  text-[10px] uppercase font-semibold ">
+              AM
+            </small>
+          </div>
+          <div className="flex gap-3 items-center">
+            <div className="bg-indigo-300 w-2 h-2"></div>
+            <small className="text-gray-400  text-[10px] uppercase font-semibold ">
+              PM
+            </small>
+          </div>
+        </div>
       </div>
       <div className="bg-white shadow-md dark:bg-gray-800 mb-8 rounded-md p-2 h">
         <table className="min-w-full  border-collapse w-full border-none">
@@ -51,7 +69,15 @@ const Timesheet = () => {
               <th
                 scope="col"
                 className="rounded-tr-md hidden md:table-cell lg:table-cell py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
-              ></th>
+              >
+                Action
+              </th>
+              <th
+                scope="col"
+                className="rounded-tr-md hidden md:table-cell lg:table-cell py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
+              >
+                Expand
+              </th>
             </tr>
           </thead>
           <tbody className="text-[11px] divide-y divide-gray-100 overflow-auto text-gray-600">
@@ -63,24 +89,42 @@ const Timesheet = () => {
                 0903000000001395
               </td>
               <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell ">
-                <span className="font-semibold text-[9px] uppercase text-blue-400">
-                  View link
+                <span className="text-[10px] uppercase">
+                  April 19, 2020 08:00 PM
                 </span>
               </td>
+              <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell ">
+                <span className="text-[10px] uppercase">
+                  April 19, 2020 12:00 PM
+                </span>
+              </td>
+
               <td className="dark:text-gray-400  px-5  py-2 hidden md:table-cell lg:table-cell ">
-                <span>8hrs14m</span>
+                <span>4</span>
               </td>
               <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
                 <div className="flex bg-gray-100 rounded items-center max-w-max px-2 py-1 gap-2">
                   <div className="w-2 h-2 bg-green-300 "></div>
-                  <span className=" text-green-500   text-[9px]">
-                    Completed
-                  </span>
+                  <span className=" text-green-500   text-[9px]">Recorded</span>
                 </div>
               </td>
               <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
                 <div className="text-gray-500 pr-2">
-                  <FiMoreHorizontal className="text-lg" />
+                  <span className="text-[9px] text-blue-400 font-semibold">
+                    UPDATE
+                  </span>
+                </div>
+              </td>
+              <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
+                <div
+                  className="cursor-pointer"
+                  onClick={() => setExpand(!expand)}
+                >
+                  {expand ? (
+                    <IoMdArrowDropdown className="text-lg" />
+                  ) : (
+                    <IoMdArrowDropup className="text-lg" />
+                  )}
                 </div>
               </td>
             </tr>
