f2f3fbcbf25c5039fa543b864f3528b6afb1eade
PaulPintang <pauljpintang@gmail.com>
2022-04-20 08:57:18 +0800

    style: new task card layout and hover effect

--- src/components/HomePage/pages/Task/index.jsx
+++ src/components/HomePage/pages/Task/index.jsx
@@ -8,7 +8,7 @@ const Task = () => {
         <div className="flex flex-col">
           <p className="text-gray-700 text-2xl">Your Task</p>
         </div>
-        <div className="py-3">
+        <div className="pt-3">
           <div className="flex flex-col md:flex-row lg:flex-row lg:justify-start lg:items-center gap-2 md:gap-4 lg:gap-4 pb-3">
             <div className="flex gap-3 items-center">
               <div className="bg-yellow-300 w-2 h-2"></div>
@@ -42,77 +42,65 @@ const Task = () => {
             </div>
           </div>
         </div>
-        {/* <div className="w-full flex justify-between items-center gap-2">
-          <div className=" bg-opacity-60 rounded-md shadow-md w-full">
-            <div className="h-[80px] text-center p-4">
-              <div className="bg-indigo-300 w-8 h-1"></div>
-              <div className="h-full flex items-center justify-between">
-                <div className="bg-gray-100 rounded-full p-2"></div>
-                <p className="text-[11px] text-gray-400">New Task</p>
-                <p className="text-[25px]">07</p>
-              </div>
-            </div>
-          </div>
-          <div className=" bg-opacity-60 rounded-md shadow-md w-full">
-            <div className="h-[80px] text-center p-4">
-              <div className="bg-blue-300 w-8 h-1"></div>
-              <div className="h-full flex items-center justify-between">
-                <div className="bg-gray-100 rounded-full p-2"></div>
-                <p className="text-[11px] text-gray-400">In Progress</p>
-                <p className="text-[25px]">01</p>
-              </div>
-            </div>
-          </div>
-          <div className=" bg-opacity-60 rounded-md shadow-md w-full">
-            <div className="h-[80px] text-center p-4">
-              <div className="bg-red-300 w-8 h-1"></div>
-              <div className="h-full flex items-center justify-between">
-                <div className="bg-gray-100 rounded-full p-2"></div>
-                <p className="text-[11px] text-gray-400">For QA</p>
-                <p className="text-[25px]">03</p>
-              </div>
-            </div>
-          </div>
-          <div className=" bg-opacity-60 rounded-md shadow-md w-full">
-            <div className="h-[80px] text-center p-4">
-              <div className="bg-red-300 w-8 h-1"></div>
-              <div className="h-full flex items-center justify-between">
-                <div className="bg-gray-100 rounded-full p-2"></div>
-                <p className="text-[11px] text-gray-400">For QA</p>
-                <p className="text-[25px]">03</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
+      </div>
       <Grid>
         <Grid.Col span={3}>
+          <div className="pb-2">
+            <small className="text-gray-400  text-[10px] uppercase font-semibold">
+              New Task
+            </small>
+          </div>
+
           <div className="flex flex-col gap-3">
-            <div className=" bg-opacity-60 rounded-md shadow-md w-full">
-              <div className="h-[80px] text-center p-4">
+            <div className=" bg-opacity-60 rounded-md shadow-md w-full cursor-pointer hover:-translate-y-0.5 transition-all">
+              <div className="p-4 space-y-2">
                 <div className="bg-indigo-300 w-8 h-1"></div>
-                <div className="h-full flex items-center justify-between">
-                  <div className="bg-gray-100 rounded-full p-2"></div>
-                  <p className="text-[11px] text-gray-400">New Task</p>
-                  <p className="text-[25px]">07</p>
-                </div>
-              </div>
-            </div>
-            <div className=" bg-opacity-60 rounded-md shadow-md w-full">
-              <div className="h-[80px] text-center p-4">
-                <div className="bg-indigo-300 w-8 h-1"></div>
-                <div className="h-full flex items-center justify-between">
-                  <div className="bg-gray-100 rounded-full p-2"></div>
-                  <p className="text-[11px] text-gray-400">New Task</p>
-                  <p className="text-[25px]">07</p>
+                <div>
+                  <p className="text-[13px] ">FE | OKR Card Component</p>
+                  <div className="flex gap-2 items-center text-[10px] text-gray-500">
+                    <p>Ticket:</p>
+                    <span>0903000000001395</span>
+                  </div>
+                  <div className="flex gap-2 items-center text-[10px] text-gray-500">
+                    <p>Added:</p>
+                    <span>April 20, 08:50 AM</span>
+                  </div>
+                  <div className="flex gap-2 items-center text-[10px] text-gray-500">
+                    <p>Deliverable Link:</p>
+                    <span className="text-blue-400 font-semibold">View</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </Grid.Col>
-        <Grid.Col span={3}>2</Grid.Col>
-        <Grid.Col span={3}>3</Grid.Col>
-        <Grid.Col span={3}>4</Grid.Col>
+        <Grid.Col span={3}>
+          <div className="pb-2">
+            <small className="text-gray-400  text-[10px] uppercase font-semibold">
+              In-Progress
+            </small>
+          </div>
+
+          <div className="flex flex-col gap-3"></div>
+        </Grid.Col>
+        <Grid.Col span={3}>
+          <div className="pb-2">
+            <small className="text-gray-400  text-[10px] uppercase font-semibold">
+              For QA
+            </small>
+          </div>
+
+          <div className="flex flex-col gap-3"></div>
+        </Grid.Col>
+        <Grid.Col span={3}>
+          <div className="pb-2">
+            <small className="text-gray-400  text-[10px] uppercase font-semibold">
+              Completed
+            </small>
+          </div>
+
+          <div className="flex flex-col gap-3"></div>
+        </Grid.Col>
       </Grid>
     </div>
   );
