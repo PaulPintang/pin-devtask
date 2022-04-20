738041800c2fbf97ae6f1f32fb3d638606a1305b
PaulPintang <pauljpintang@gmail.com>
2022-04-20 11:39:37 +0800

    feat: install mantine/date

--- src/components/HomePage/pages/TimeSheet/index.jsx
+++ src/components/HomePage/pages/TimeSheet/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import { Grid, Input } from "@mantine/core";
+import { TimeInput } from "@mantine/dates";
 const Timesheet = () => {
   const [expand, setExpand] = useState(false);
 
@@ -25,112 +27,132 @@ const Timesheet = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white shadow-md dark:bg-gray-800 mb-8 rounded-md p-2 h">
-        <table className="min-w-full  border-collapse w-full border-none">
-          <thead className="sticky top-0 z-30  dark:bg-[#2D3746]">
-            <tr>
-              <th
-                scope="col"
-                className="md:px-3 lg:px-3 pl-3 py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider  bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm rounded-tl-md"
-              >
-                Task Name
-              </th>
-              <th
-                scope="col"
-                className="rounded-tr-md md:rounded-none lg:rounded-none py-3 md:pr-3 lg:pr-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider  bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
-              >
-                Ticket No.
-              </th>
-              <th
-                scope="col"
-                className="hidden md:table-cell lg:table-cell  py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
-              >
-                Start Date Time
-              </th>
-              <th
-                scope="col"
-                className="hidden md:table-cell lg:table-cell px-5 py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
-              >
-                End Date Time
-              </th>
+      <Grid grow gutter="sm" className="h-full ">
+        <Grid.Col span={8} className="h-2/4 ">
+          <div className="bg-white shadow-md dark:bg-gray-800 mb-8 rounded-md p-2 h">
+            <table className="min-w-full  border-collapse w-full border-none">
+              <thead className="sticky top-0 z-30  dark:bg-[#2D3746]">
+                <tr>
+                  <th
+                    scope="col"
+                    className="md:px-3 lg:px-3 pl-3 py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider  bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm rounded-tl-md"
+                  >
+                    Task Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="rounded-tr-md md:rounded-none lg:rounded-none py-3 md:pr-3 lg:pr-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider  bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
+                  >
+                    Ticket No.
+                  </th>
+                  {/* <th
+                    scope="col"
+                    className="hidden md:table-cell lg:table-cell  py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
+                  >
+                    Start Date Time
+                  </th>
+                  <th
+                    scope="col"
+                    className="hidden md:table-cell lg:table-cell px-5 py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
+                  >
+                    End Date Time
+                  </th> */}
 
-              <th
-                scope="col"
-                className=" hidden md:table-cell lg:table-cell py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
-              >
-                Spent
-              </th>
-              <th
-                scope="col"
-                className=" hidden md:table-cell lg:table-cell py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
-              >
-                Status
-              </th>
-              <th
-                scope="col"
-                className="rounded-tr-md hidden md:table-cell lg:table-cell py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
-              >
-                Action
-              </th>
-              <th
-                scope="col"
-                className="rounded-tr-md hidden md:table-cell lg:table-cell py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
-              >
-                Expand
-              </th>
-            </tr>
-          </thead>
-          <tbody className="text-[11px] divide-y divide-gray-100 overflow-auto text-gray-600">
-            <tr className="border-none ">
-              <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell pl-3">
-                FE | BE | Todo App
-              </td>
-              <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
-                0903000000001395
-              </td>
-              <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell ">
-                <span className="text-[10px] uppercase">
-                  April 19, 2020 08:00 PM
-                </span>
-              </td>
-              <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell ">
-                <span className="text-[10px] uppercase">
-                  April 19, 2020 12:00 PM
-                </span>
-              </td>
+                  <th
+                    scope="col"
+                    className=" hidden md:table-cell lg:table-cell py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
+                  >
+                    Spent
+                  </th>
+                  <th
+                    scope="col"
+                    className=" hidden md:table-cell lg:table-cell py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="rounded-tr-md hidden md:table-cell lg:table-cell py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
+                  >
+                    Action
+                  </th>
+                  <th
+                    scope="col"
+                    className="rounded-tr-md hidden md:table-cell lg:table-cell py-3 text-left text-[9px] font-[600] text-gray-400 dark:text-gray-50  tracking-wider bg-gray-100 dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
+                  >
+                    Expand
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="text-[11px] divide-y divide-gray-100 overflow-auto text-gray-600">
+                <tr className="border-none ">
+                  <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell pl-3">
+                    FE | BE | Todo App
+                  </td>
+                  <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
+                    0903000000001395
+                  </td>
+                  <td className="dark:text-gray-400  px-5  py-2 hidden md:table-cell lg:table-cell ">
+                    <span>4</span>
+                  </td>
+                  <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
+                    <div className="flex bg-gray-100 rounded items-center max-w-max px-2 py-1 gap-2">
+                      <div className="w-2 h-2 bg-green-300 "></div>
+                      <span className=" text-green-500   text-[9px]">
+                        Recorded
+                      </span>
+                    </div>
+                  </td>
+                  <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
+                    <div className="text-gray-500 pr-2">
+                      <span className="text-[9px] text-blue-400 font-semibold">
+                        UPDATE
+                      </span>
+                    </div>
+                  </td>
+                  <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
+                    <div
+                      className="cursor-pointer"
+                      onClick={() => setExpand(!expand)}
+                    >
+                      {expand ? (
+                        <IoMdArrowDropdown className="text-lg" />
+                      ) : (
+                        <IoMdArrowDropup className="text-lg" />
+                      )}
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </Grid.Col>
 
-              <td className="dark:text-gray-400  px-5  py-2 hidden md:table-cell lg:table-cell ">
-                <span>4</span>
-              </td>
-              <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
-                <div className="flex bg-gray-100 rounded items-center max-w-max px-2 py-1 gap-2">
-                  <div className="w-2 h-2 bg-green-300 "></div>
-                  <span className=" text-green-500   text-[9px]">Recorded</span>
+        <Grid.Col span={4} className="h-2/4">
+          <div className="w-full h-full pr-3 shadow-md rounded-md">
+            <div className="p-3">
+              <p className="text-gray-700">FE | OKR Card Component </p>
+              <span className="text-[11px] text-gray-400 ">
+                Note: Task here is based on your on-progress task
+              </span>
+              <div>
+                <Input placeholder="Ticket Number" size="xs" />
+                <div>
+                  <p>Start Date Time</p>
+                  <div className="flex gap-2">
+                    <Input placeholder="Ticket Number" size="xs" />
+                    <TimeInput
+                      label="Pick time"
+                      format="12"
+                      defaultValue={new Date()}
+                    />
+                  </div>
                 </div>
-              </td>
-              <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
-                <div className="text-gray-500 pr-2">
-                  <span className="text-[9px] text-blue-400 font-semibold">
-                    UPDATE
-                  </span>
-                </div>
-              </td>
-              <td className="dark:text-gray-400 py-2 hidden md:table-cell lg:table-cell">
-                <div
-                  className="cursor-pointer"
-                  onClick={() => setExpand(!expand)}
-                >
-                  {expand ? (
-                    <IoMdArrowDropdown className="text-lg" />
-                  ) : (
-                    <IoMdArrowDropup className="text-lg" />
-                  )}
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+              </div>
+            </div>
+          </div>
+        </Grid.Col>
+      </Grid>
     </div>
   );
 };
