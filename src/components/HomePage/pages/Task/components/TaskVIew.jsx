0989dc0ceac3c9b94a156a78251b73736a29b9c2
PaulPintang <pauljpintang@gmail.com>
2022-04-21 21:26:02 +0800

    feat: taskview component

---
+++ src/components/HomePage/pages/Task/components/TaskVIew.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const TaskView = () => {
+  return (
+    <div className="p-3">
+      <div>
+        <p className="text-[12px] text-gray-400">Monday, April 18</p>
+        <div className="flex flex-col">
+          <p className="text-gray-700 text-2xl">Your Task</p>
+        </div>
+        <div className="pt-3">
+          <div className="flex flex-col md:flex-row lg:flex-row lg:justify-start lg:items-center gap-2 md:gap-4 lg:gap-4 pb-3">
+            <div className="flex gap-3 items-center">
+              <div className="bg-yellow-300 w-2 h-2"></div>
+              <small className="text-gray-400  text-[10px] uppercase font-semibold ">
+                All
+              </small>
+            </div>
+            <div className="flex gap-3 items-center">
+              <div className="bg-indigo-300 w-2 h-2"></div>
+              <small className="text-gray-400  text-[10px] uppercase font-semibold ">
+                New
+              </small>
+            </div>
+            <div className="flex gap-3 items-center">
+              <div className="w-2 h-2 bg-blue-300"></div>
+              <small className="text-gray-400  text-[10px] uppercase font-semibold ">
+                In-Progress
+              </small>
+            </div>
+            <div className="flex gap-3 items-center">
+              <div className="bg-green-300 w-2 h-2"></div>
+              <small className="text-gray-400  text-[10px] uppercase font-semibold ">
+                Completed
+              </small>
+            </div>
+            <div className="flex gap-3 items-center">
+              <div className="bg-red-300 w-2 h-2"></div>
+              <small className="text-gray-400  text-[10px] uppercase font-semibold ">
+                For QA
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskView;
