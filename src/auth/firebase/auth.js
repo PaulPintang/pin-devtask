dbd7757f985bbc9a109c997e635cf9635f8a116f
PaulPintang <pauljpintang@gmail.com>
2022-04-22 22:40:10 +0800

    feat: firebase configuration

---
+++ src/auth/firebase/auth.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAhav59Y_kS6XhNrGVCSf55b5sL_VyUcQ",
+  authDomain: "idevtask--auth.firebaseapp.com",
+  projectId: "idevtask--auth",
+  storageBucket: "idevtask--auth.appspot.com",
+  messagingSenderId: "37200000588",
+  appId: "1:37200000588:web:1c65c87d9ad1449886e6e9",
+};
+
+const app = initializeApp(firebaseConfig);
