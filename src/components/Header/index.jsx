54fa41fe007e3c145d1b9d08afa3dabad018a840
PaulPintang <pauljpintang@gmail.com>
2022-04-24 13:24:07 +0800

    style: navbar menu when user is logged in

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Button } from "@mantine/core";
+import { RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+import { BsBell } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 const Header = () => {
+  const [show, setShow] = useState(false);
+
   const { signOutUser, isAuth, userPic } = useContext(AuthContext);
   return (
     <div className="flex justify-between items-center h-16">
@@ -27,15 +31,41 @@ const Header = () => {
           </>
         ) : (
           <>
-            <div className="w-[30px]">
-              <img src={userPic} alt="" className="w-full rounded-full" />
+            <div className="pr-3 cursor-pointer">
+              <BsBell className="text-gray-400 opacity-70" />
             </div>
-            <p
-              onClick={signOutUser}
-              className="cursor-pointer hover:text-blue-500"
+            <div
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={() => setShow(!show)}
             >
-              Logout
-            </p>
+              <div className="w-[30px]">
+                <img src={userPic} alt="" className="w-full rounded-full" />
+              </div>
+
+              <div className="relative">
+                <div className="flex items-center gap-2 cursor-pointer">
+                  {show ? (
+                    <RiArrowUpSLine className="text-gray-300  text-[18px]" />
+                  ) : (
+                    <RiArrowDownSLine className="text-gray-300 text-[18px]" />
+                  )}
+                </div>
+                {show && (
+                  <div className="absolute z-50 right-[-6px] top-6 ">
+                    <div className="shadow-md bg-white p-3 flex flex-col justify-center items-center rounded">
+                      <div className="">
+                        <button
+                          onClick={signOutUser}
+                          className="text-[10px] font-semibold text-gray-500 uppercase hover:text-blue-300 transition-all"
+                        >
+                          Logout
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
           </>
         )}
       </div>
