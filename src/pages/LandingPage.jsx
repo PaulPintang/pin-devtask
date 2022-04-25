d19084d33bb82859c43aef12f470cd3d1baffc5e
PaulPintang <pauljpintang@gmail.com>
2022-04-25 10:50:45 +0800

    feat: create team modal component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,12 @@
 import React, { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
-import { Container, Button, Input } from "@mantine/core";
+import { Container, Button, Input, Modal } from "@mantine/core";
+import { TimeInput, DatePicker } from "@mantine/dates";
+
 import webdev from "../assets/webdev.png";
 import Header from "../components/Header";
 const LandingPage = () => {
+  const [createTeam, setCreateTeam] = useState(false);
   const [codeInput, setCodeInput] = useState(false);
   const [inputVal, setInputVal] = useState();
 
@@ -37,7 +40,11 @@ const LandingPage = () => {
               <div className="flex items-center gap-3 pr-2">
                 {isAuth ? (
                   <>
-                    <Button color="indigo" size="md">
+                    <Button
+                      onClick={() => setCreateTeam(true)}
+                      color="indigo"
+                      size="md"
+                    >
                       Create team
                     </Button>
                     <Input
@@ -52,7 +59,6 @@ const LandingPage = () => {
                         className="transition-all"
                         variant="subtle"
                         size="md"
-                        loading
                       >
                         Join
                       </Button>
@@ -81,6 +87,70 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
+      {/* Create Team Modal */}
+      <Modal opened={createTeam} withCloseButton={false}>
+        <div className="p-3">
+          <p className="text-gray-700">Create Team</p>
+          <span className="text-[11px] text-gray-400 ">
+            Please fill out the following input to get started!
+          </span>
+
+          <div className="py-2 space-y-3">
+            <div className="space-y-1">
+              <p className="text-[10px] text-gray-400 uppercase font-semibold">
+                Company Name
+              </p>
+              <Input placeholder="Task name" size="xs" />
+            </div>
+            <div className="space-y-1">
+              <p className="text-[10px] text-gray-400 uppercase font-semibold">
+                Ticket No.
+              </p>
+              <Input placeholder="Ticket number" size="xs" />
+            </div>
+            <div className="space-y-1">
+              <p className="text-[10px] text-gray-400 uppercase font-semibold">
+                Start Date Time
+              </p>
+              <div className="flex justify-between gap-2">
+                <DatePicker
+                  className="w-2/4"
+                  size="xs"
+                  placeholder="Pick date"
+                  defaultValue={new Date()}
+                  disabled
+                />
+                <TimeInput
+                  className="w-2/4"
+                  size="xs"
+                  format="12"
+                  defaultValue={new Date()}
+                  disabled
+                />
+              </div>
+            </div>
+            <div className="space-y-1">
+              <p className="text-[10px] text-gray-400 uppercase font-semibold">
+                End Date Time
+              </p>
+              <div className="flex justify-between gap-2">
+                <DatePicker
+                  className="w-2/4"
+                  size="xs"
+                  placeholder="Pick date"
+                  defaultValue={new Date()}
+                  disabled
+                />
+                <TimeInput className="w-2/4" size="xs" format="12" />
+              </div>
+            </div>
+          </div>
+
+          <Button mt={3} color="indigo" fullWidth>
+            Start the task
+          </Button>
+        </div>
+      </Modal>
     </Container>
   );
 };
