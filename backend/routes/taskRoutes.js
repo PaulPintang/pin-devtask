db0e1b7640935be44f1444d5a0c4432079782e4b
PaulPintang <pauljpintang@gmail.com>
2022-05-05 10:44:04 +0800

    feat: handlesubmit

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { getTask, addTask } = require("../controllers/taskController");
+
+// Request
+router.route("/").get(getTask).post(addTask);
+// router.route("/:id").put(updateTodo).delete(deleteTodo);
+
+module.exports = router;
