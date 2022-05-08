bf9d9b9177034a9114e0ff09165cf73d4b6a578c
PaulPintang <pauljpintang@gmail.com>
2022-05-08 13:33:29 +0800

    feat: admin, add menu for intern page

--- client/src/components/HomePage/admin/Navbar.jsx
+++ client/src/components/HomePage/admin/Navbar.jsx
@@ -54,6 +54,15 @@ const Navbar = () => {
                 <p className="text-gray-700 text-[12.2px]">All Task</p>
               </div>
             </Link>
+            <Link to="/manage-intern">
+              <div
+                className="flex items-center gap-3 w-full bg- gray-100 px-2 py-2 hover:bg-gray-100 cursor-pointer transition-all rounded-md 
+                "
+              >
+                <VscTasklist className="text-gray-400" />
+                <p className="text-gray-700 text-[12.2px]">Intern</p>
+              </div>
+            </Link>
             {/* <Link to="/performance" style={{ textDecoration: "none" }}>
               <div className="flex items-center gap-3 w-full bg -gray-100 px-2 py-2 hover:bg-gray-100 cursor-pointer transition-all rounded-md">
                 <BiMessageRounded className="text-gray-400 text-lg" />
