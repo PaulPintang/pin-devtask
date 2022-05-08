b6ec381ae2c97e489bb31938a09714ea09ba4c4f
PaulPintang <pauljpintang@gmail.com>
2022-05-08 13:30:01 +0800

    feat: admin, manage intern page

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getTask, addTask } = require("../controllers/taskController");
 
-// Request
+//  Request
 router.route("/").get(getTask).post(addTask);
 // router.route("/:id").put(updateTodo).delete(deleteTodo);
 
---
+++ client/src/components/HomePage/admin/pages/Intern/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Intern = () => {
+  return <div>manage intern page</div>;
+};
+
+export default Intern;
